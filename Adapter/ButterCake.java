b5d65b44c8a17e14badceb413c4053577df86443
linabadawi <56623261+linabadawi@users.noreply.github.com>
2020-05-20 15:45:18 +0300

    Add files via upload

---
+++ Adapter/ButterCake.java
@@ -0,0 +1,18 @@
+package Adapter;
+
+public class ButterCake implements cake{
+
+	@Override
+	public void falvor() {
+		// TODO Auto-generated method stub
+		System.out.println("chocolate");
+	}
+
+	@Override
+	public void price( ) {
+		// TODO Auto-generated method stub
+		System.out.println("8.jd");
+	}
+
+	
+}
---
+++ Adapter/redVelvet.java
@@ -0,0 +1,10 @@
+package Adapter
+;
+
+public interface redVelvet {
+	 // target interface 
+    // toyducks dont fly they just make 
+    // squeaking sound 
+   
+	public void prices(); 
+}
